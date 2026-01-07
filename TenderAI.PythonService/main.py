ffefd546ae04cb883c8beda7bc78c6b2336d01cd
Imouse <initin.net@gmail.com>
2026-01-07 12:49:07 +0530

    Added docuemnt download and text fetching in python worker

--- TenderAI.PythonService/main.py
+++ TenderAI.PythonService/main.py
@@ -1,12 +1,22 @@
-import os,pika,json,sys
+import os,pika,json,sys,fitz
+from minio import Minio
+
 
 def main():
 
     # Establising the connection and channel
     credential = pika.PlainCredentials(username='guest',password='1AVWTEyHt77pH7dBsj140P')
-    connection_parameters = pika.ConnectionParameters(host='localhost',port=58943,credentials=credential)
+    connection_parameters = pika.ConnectionParameters(host='localhost',port=50343,credentials=credential)
     connection = pika.BlockingConnection(parameters=connection_parameters)
     channel = connection.channel()
+
+    minio_client = Minio(
+        endpoint= "localhost:50336",
+        access_key= "minioadmin",
+        secret_key="F7gmz*p~v)5uEN{Kc~7UqP",
+        secure=False
+    )
+
 
     #Setting up exchnage
     exchange_name = 'TenderAI.ApiService.Contracts:TenderUploaded'
@@ -22,7 +32,25 @@ def main():
 
     #Callback method to receive messages continously
     def callback(ch,method,properties,body):
-        print(f'[x] Received : {body}')
+        json_string = body.decode()
+        data = json.loads(json_string)
+        actual_message = data['message']
+
+        file_name = actual_message['fileName']
+        file_id = actual_message['fileId']
+        print(f'[x] File Name : {file_name} \n[x] File Id : {file_id}')
+
+        download_folder ="downloads"
+        os.makedirs(download_folder,exist_ok=True)
+        local_path = os.path.join(download_folder,file_name)
+
+        minio_client.fget_object(bucket_name='tender-uploads',object_name=file_name,file_path=local_path)
+        print(f'\nFile downloaded successfully {file_name}')
+
+        with fitz.open(local_path) as doc:
+            first_page = doc[0]
+            text =first_page.get_text()
+            print(f"\n Text : {text}")
 
     channel.basic_consume(queue=queue_name,on_message_callback=callback,auto_ack=True)
     channel.start_consuming()
